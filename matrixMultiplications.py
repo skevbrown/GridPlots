8eb8e36df9e3f1e2b9555ea467a1cff40f895612
skevbrown <35709674+skevbrown@users.noreply.github.com>
2018-01-23 17:00:56 -0700

    added mattix multiplication file

---
+++ matrixMultiplications.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+matrixA = np.zeros([4,4])
+matrixA[0,:] = [-2,1,0,1];
+matrixA[1,:] = [1,-2,1,0]
+matrixA[2,:] = [0,1,-2,1]
+matrixA[3,:] = [1,2,0,-2]
+
+
+
+print(matrixA)
