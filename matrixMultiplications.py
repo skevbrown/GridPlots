d11b2c4bf8cabf82333cddd4be1a9c232d92dd41
skevbrown <35709674+skevbrown@users.noreply.github.com>
2018-01-23 18:04:53 -0700

    replaced muliplcation with numpy dot

--- matrixMultiplications.py
+++ matrixMultiplications.py
@@ -6,8 +6,8 @@ matrixA = np.zeros([4,4])
 matrixA[0,:] = [-2,1,0,1];
 matrixA[1,:] = [1,-2,1,0]
 matrixA[2,:] = [0,1,-2,1]
-matrixA[3,:] = [1,2,0,-2]
+matrixA[3,:] = [1,0,1,-2]
 
+vec1= np.array([-2,1,0,1])
 
-
-print(matrixA)
+print( matrixA.dot(vec1) )
